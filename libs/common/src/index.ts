6e889d6d6f716dd21d4e241c33a614020da61c0b
Saeed Norouzi <saeednorouzi98@gmail.com>
2025-05-02 21:00:12 +0330

    Remove(libs): Remove unneeded files
    Description:
    Remove some of the unneeded index files that was only making the files structure complicated and confusing.

--- libs/common/src/index.ts
+++
@@ -1,7 +0,0 @@
-export * as filter from './filters';
-export * as pipe from './pipe';
-export * as interceptor from './interceptor';
-export * as middleware from './middlewares';
-export * as utilities from './utilities';
-export * as config from './config';
-export * as responses from './responses';
--- libs/common/src/utilities/index.ts
+++
@@ -1,4 +0,0 @@
-export * as pagination from './pagination';
-export * as sanitizer from './sanitizer';
-export * as multer from './multer';
-export * as functions from './functions';
